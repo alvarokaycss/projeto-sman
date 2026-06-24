d5a453873df34a9af6cfe7bab3eede5cbff506e3
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2026-06-24 11:01:05 -0300

    refactor(frontend): permite a aplicação rodar fora do docker

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // 1. Permite que o Vite responda a requisições de fora do container
+    host: true,
+    // 2. Resolve o problema do WebSocket do HMR com Docker
+    port: 5173,
+    // 3. Força o Vite a verificar mudanças ativamente (Polling)
+    watch: {
+      usePolling: true,
+    },
+  },
+});
