1133ed6af807afe2e4992e60853752f41f24b85a
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2026-06-26 10:26:48 -0300

    test(frontend): adiciona placeholders para teste da responsividade

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -7,24 +7,32 @@ function App() {
       <main className="dashboard">
         <section className="generalSection">
           <div className="generalGauge">
+            <div className="placeholder-gauge">Gauge Principal (1:1)</div>
           </div>
           <History />
         </section>
         <section className="specificSection">
           <div className="timelineChart">
+            <div className="placeholder-chart">Gráfico de Linha Temporal</div>
           </div>
           <div className="gaugesGrid">
             <div className="gauge">
+              <div className="placeholder-gauge">Gauge 1</div>
             </div>
             <div className="gauge">
+              <div className="placeholder-gauge">Gauge 2</div>
             </div>
             <div className="gauge">
+              <div className="placeholder-gauge">Gauge 3</div>
             </div>
             <div className="gauge">
+              <div className="placeholder-gauge">Gauge 4</div>
             </div>
             <div className="gauge">
+              <div className="placeholder-gauge">Gauge 5</div>
             </div>
             <div className="gauge">
+              <div className="placeholder-gauge">Gauge 6</div>
             </div>
           </div>
         </section>
