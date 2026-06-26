6c91f97550d1e267c32a1d7f1f2e3f9ac08ad397
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2026-06-26 10:24:44 -0300

    feat(frontend): adiciona html da estrutura base ao app

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,7 +1,36 @@
+import { History } from "./components/History";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <main className="dashboard">
+        <section className="generalSection">
+          <div className="generalGauge">
+          </div>
+          <History />
+        </section>
+        <section className="specificSection">
+          <div className="timelineChart">
+          </div>
+          <div className="gaugesGrid">
+            <div className="gauge">
+            </div>
+            <div className="gauge">
+            </div>
+            <div className="gauge">
+            </div>
+            <div className="gauge">
+            </div>
+            <div className="gauge">
+            </div>
+            <div className="gauge">
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
